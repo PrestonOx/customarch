7e259a712b219e16833cbea8dcea8b076afb319a
PrestonOx <94470061+PrestonOx@users.noreply.github.com>
2021-11-18 18:01:17 +0100

    Add files via upload

---
+++ part.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sectorsize=`fdisk -l | head -n 3 | grep Units: | awk {'print $6'}`
+diskname=`fdisk -l | head -n 1 | awk {'print $2'} |sed 's/.$//'`
+
+sizeboot=$((250000000  / $sectorsize))
+disksize=`fdisk -l | head -n 1 | awk {'print $5'}`
+ramsize=`free -b | grep Mem: | awk {'print $2'}`
+firststart='2048'
+#framsize=$(($ramsize / $sectorsize))
+fstart=$(($sizeboot + $firststart))
+secramsize=$(($ramsize / $sectorsize))
+secdisksize=$(($disksize / $sectorsize))
+secdiska=$(($secdisksize - $secramsize - $sizeboot))
+secdiskroot=$(($secdiska / 2))
+secdiskhome=$(($secdiskroot - $firststart))
+secstarts=$(($secramsize + $fstart))
+secstartt=$(($secstarts + $secdiskroot))
+
+#Redirection vers le fichier my.layout
+
+echo "label: dos" > my.layout
+#echo "device: /dev/sda" >> my.layout
+echo "device: $diskname" >> my.layout
+echo "unit: sectors" >> my.layout
+#echo "sector-size: 512" >> my.layout
+echo "sector-size: $sectorsize" >> my.layout
+echo ""$diskname"1 : start= $firststart, size= $sizeboot, type=83, bootable" >> my.layout
+echo ""$diskname"2 : start= $fstart, size= $secramsize, type=82" >> my.layout
+echo ""$diskname"3 : start= $secstarts, size= $secdiskroot, type=83" >> my.layout
+echo ""$diskname"4 : start= $secstartt, size= $secdiskhome, type=83" >> my.layout
