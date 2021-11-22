c7b157254daecdd791134535dd55a304dda86070
PrestonOx <94470061+PrestonOx@users.noreply.github.com>
2021-11-22 14:35:42 +0100

    Update part_with_lvm2.sh

--- part_with_lvm2.sh
+++ part_with_lvm2.sh
@@ -52,16 +52,16 @@ lvcreate -l 50%FREE VolGroup1 -n lvolhome
 
 #Formatage de nos volumes logiques (ainsi que de la partition primaire "boot")
 
-#mkfs.fat -F 32 /dev/"$diskname"1
+#mkfs.fat -F32 /dev/"$diskname"1
 #mkfs.ext4 /dev/VolGroup1/lvswap
 #mkfs.ext4 /dev/VolGroup1/lvolroot
 #mkfs.ext4 /dev/VolGroup1/lvolhome
 
 #mkswap /dev/VolGroup1/lvswap
+#swapon /dev/VolGroup1/lvswap
 
 #mount /dev/VolGroup1/lvolroot /mnt
 #mkdir /mnt/home
 #mount /dev/VolGroup1/lvolhome /mnt/home
 #mkdir /mnt/boot
 #mount /dev/"$diskname"1 /mnt/boot
-#swapon /dev/VolGroup1/lvswap
