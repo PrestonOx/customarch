5263d3b217da765fd95c95f24b29fa27386aa202
PrestonOx <94470061+PrestonOx@users.noreply.github.com>
2021-11-22 16:53:01 +0100

    Add files via upload

---
+++ install_archlinux_lvm_part1.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#Install ArchLinux
+#Changement de langues, timezone...
+loadkeys fr
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Paris
+
+sectorsize=`fdisk -l | head -n 3 | grep Units: | awk {'print $6'}`
+diskname=`fdisk -l | head -n 1 | awk {'print $2'} |sed 's/.$//'`
+
+#sizeboot=$((250000000  / $sectorsize))
+sizeboot=$((512000000  / $sectorsize))
+disksize=`fdisk -l | head -n 1 | awk {'print $5'}`
+ramsize=`free -b | grep Mem: | awk {'print $2'}`
+firststart='2048'
+fstart=$(($sizeboot + $firststart))
+secramsize=$(($ramsize / $sectorsize))
+secdisksize=$(($disksize / $sectorsize))
+secdiska=$(($secdisksize - $secramsize - $sizeboot))
+secdiskroot=$(($secdiska / 2))
+secdiskhome=$(($secdiskroot - $firststart))
+secstarts=$(($secramsize + $fstart))
+secstartt=$(($secstarts + $secdiskroot))
+secdiskav=$(($secdisksize - $sizeboot))
+secdiskavailable=$(($secdiskav - $firststart))
+
+#Redirection vers le fichier my.layout
+
+lastsector=`fdisk -l | head -n 1 | awk {'print $7'}`
+sizefor2=$(($lastsector - $fstart - 34))
+last_lba=$(($lastsector - 34))
+
+echo "label: gpt" > my.layout
+echo "device: $diskname" >> my.layout
+echo "unit: sectors" >> my.layout
+echo "first-lba: 2048" >> my.layout
+echo "last-lba: $last_lba" >> my.layout
+echo "sector-size: $sectorsize" >> my.layout
+echo ""$diskname"1 : start= $firststart, size= $sizeboot, type= C12A7328-F81F-11D2-BA4B-00A0C93EC93B" >> my.layout
+echo ""$diskname"2 : start= $fstart, size= $sizefor2, type= 0FC63DAF-8483-4772-8E79-3D69D8477DE4" >> my.layout
+
+sfdisk $diskname < my.layout
+
+#Z'est barti pour le LVM
+
+pvcreate ""$diskname"2"
+
+vgcreate VolGroup1 ""$diskname"2"
+
+lvcreate -L "$ramsize"B VolGroup1 -n lvswap
+lvcreate -l 50%FREE VolGroup1 -n lvolroot
+lvcreate -l 100%FREE VolGroup1 -n lvolhome
+
+#Formatage de nos volumes logiques (ainsi que de la partition primaire "boot")
+
+mkfs.fat -F32 /dev/""$diskname"1"
+mkfs.ext4 /dev/VolGroup1/lvswap
+mkfs.ext4 /dev/VolGroup1/lvolroot
+mkfs.ext4 /dev/VolGroup1/lvolhome
+
+mkswap /dev/VolGroup1/lvswap
+swapon /dev/VolGroup1/lvswap
+
+mount /dev/VolGroup1/lvolroot /mnt
+mkdir /mnt/home
+mount /dev/VolGroup1/lvolhome /mnt/home
+mkdir /mnt/boot
+mount ""$diskname"1" /mnt/boot
+
+#Installation des paquets
+pacstrap /mnt base base-devel linux linux-firmware efibootmgr vim nano dhcpcd man-db lvm2
+
+#FSTAB
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+cp install_archlinux_lvm_part2.sh /mnt/install_archlinux_lvm_part2.sh
+arch-chroot /mnt ./install_archlinux_lvm_part2.sh
+reboot
---
+++ install_archlinux_lvm_part2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Dernière ligne droite
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+#Localization
+
+
+#echo "fr_FR.UTF-8 UTF-8" >> /etc/locale.gen
+#echo "en_EN.UTF-8 UTF-8" >> /etc/locale.gen
+sed -i 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g'
+sed -i 's/#en_EN.UTF-8 UTF-8/en_EN.UTF-8 UTF-8/g'
+locale-gen
+echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=fr" >> /etc/vconsole.conf
+
+#Hostname
+
+echo "generichostname" >> /etc/hostname
+
+#mkinitcpio (rajouter les hooks essentiels)
+# sed -re 's/^HOOKS=(.*)$/^HOOKS=(.*keymap lvm2)$/g' /etc/mkinitcpio.conf
+
+sed -re 's/block/block lvm2/g' /etc/mkinitcpio.conf
+sed -re 's/keyboard/keyboard keymap/g' /etc/mkinitcpio.conf
+
+mkinitcpio -P
+
+#Definir un mot de passe root
+echo -e "testdeploy\ntestdeploy" | passwd
+
+#Install GRUB
+pacman -S --noconfirm grub
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable dhcpcd
+
+#Redemarrage
+exit
